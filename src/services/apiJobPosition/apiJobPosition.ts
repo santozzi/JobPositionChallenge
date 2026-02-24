d5eb24a7d87414f2e775f2f238d401cbefc7398f
Sergio J. Antozzi <santozzi@gmail.com>
2026-02-24 07:43:57 -0300

    manejo de errores en apiJobPosition

--- src/services/apiJobPosition/apiJobPosition.ts
+++ src/services/apiJobPosition/apiJobPosition.ts
@@ -14,59 +14,58 @@ export type Candidato = {
   email: string;
 };
 export type Postulacion = {
-  id: number, 
-  title: string
-}
+  id: number;
+  title: string;
+};
 
-export async function obtenerDatosDeCandidato(email: string):Promise<Candidato> {
-
+export async function obtenerDatosDeCandidato(
+  email: string,
+): Promise<Candidato> {
   if (!email) {
     throw new Error("Falta el email del candidato");
   }
 
-  //TODO: No pasa por aca, analizar
-  
-  if(!esEmailValido(email)){
-
-    
+  if (!esEmailValido(email)) {
     throw new Error("Formato de email incorrecto");
   }
 
- 
   const data = await fetch(
     `${BASE_URL}/api/candidate/get-by-email?email=${email}`,
   );
-  if(data.status === 404){
-    throw new NotFoundCandidateException("No se encontró un candidato con ese email")
-  }
-  if(!data.ok){
-     throw new Error("Error inesperado");
+
+  if (!data.ok) {
+    if (data.status === 404) {
+      throw new NotFoundCandidateException(
+        "No se encontró un candidato con ese email",
+      );
+    } else {
+      throw new Error("Error inesperado");
+    }
   }
   const datosDelCandidato = await data.json();
-   
- 
+
   return datosDelCandidato as Candidato;
-
-
 }
-export async function obtenerPostulacionesDisponibles():Promise<Postulacion[]> {
 
+export async function obtenerPostulacionesDisponibles(): Promise<
+  Postulacion[]
+> {
+  const data = await fetch(`${BASE_URL}/api/jobs/get-list`);
+  if (!data.ok) {
+    throw new Error("error inesperado");
+  }
 
-  const data = await fetch(
-    `${BASE_URL}/api/jobs/get-list`,
-  );
-  const datosDePostulaciones= await data.json();
-   
-  if(datosDePostulaciones.length == 0){
-      //Esto lo hago para desacoplar los errores de la api con los que recibe la aplicacion, mañana cambio de api lo cambio de aca y la app ni enterada.
-      throw new EmptyOpenPositionListException("No se encontraron posiciones abiertas")
+  const datosDePostulaciones = await data.json();
+
+  if (datosDePostulaciones.length == 0) {
+    throw new EmptyOpenPositionListException(
+      "No se encontraron posiciones abiertas",
+    );
   }
   return datosDePostulaciones as Postulacion[];
-
-
 }
 
-export async function enviarPostulacion(apply:Apply):Promise<boolean>{
+export async function enviarPostulacion(apply: Apply): Promise<boolean> {
   const data = await fetch(`${BASE_URL}/api/candidate/apply-to-job`, {
     method: "POST",
     headers: {
@@ -74,17 +73,19 @@ export async function enviarPostulacion(apply:Apply):Promise<boolean>{
     },
     body: JSON.stringify({...apply}),
   });
+  
+ 
 
-  console.log("en enviar", JSON.stringify({...apply}));
-  const error = await data.json();
 
   if (!data.ok) {
-    
-    console.log(error);
-    
-   return false;
+    if(data.status === 400){
+      throw new Error("Solicitud incorrecta, error interno");
+    }else{
+      throw new Error("Error inesperado");
+    }
   }
+  
+  
 
-    console.log(error);
   return true;
 }
